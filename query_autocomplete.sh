66a418d24637962a6e8b298d25ae6d01fb3a3308
dimonchikakus <dimonchikakus.gmail.com>
2024-10-27 17:33:50 +0200

    work

--- query_autocomplete.sh
+++ query_autocomplete.sh
@@ -2,12 +2,20 @@
 
 QUERY=$1
 
+# Determine fuzziness level based on word length
+WORD_LENGTH=${#QUERY}
+if (( WORD_LENGTH > 7 )); then
+  FUZZINESS=3
+else
+  FUZZINESS="AUTO"
+fi
+
 curl -X GET "localhost:9200/autocomplete/_search" -H 'Content-Type: application/json' -d "{
   \"query\": {
     \"match\": {
       \"word\": {
         \"query\": \"$QUERY\",
-        \"fuzziness\": \"AUTO\",
+        \"fuzziness\": \"$FUZZINESS\",
         \"prefix_length\": 1
       }
     }
